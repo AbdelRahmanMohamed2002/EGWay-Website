6fcba851f157927ed850d7298d425e92c70ff0af
AbdelRahmanMohamed2002 <105849197+AbdelRahmanMohamed2002@users.noreply.github.com>
2022-06-07 18:30:48 +0200

    Update signin.php

--- signin.php
+++ signin.php
@@ -1,3 +1,4 @@
+
 <style>
     .container {
   padding: 16px;
@@ -54,24 +55,78 @@ a {
 <html>
     
     <body>
-<form action=".php" method="post" >
+<form action="signin12.php" method="post"  onsubmit="return SIG()" >
   <div class="container">
     <h1>Signin</h1>
     <hr>
 
-    <label for="email"><b>Email</b></label>
+    <label for="email1"><b>Email</b></label>
     <input type="email" placeholder="Enter Email" name="email" id="email" required>
-    <label for="psw"><b>Password</b></label>
+    <label for="psw1"><b>Password</b></label>
     <input type="password" placeholder="Enter Password" name="psw" id="psw" required>
-    <button type="submit" class="signinbtn" name="submit">Signin</button>
+    <button type="submit" class="signinbtn" name="submit" >Signin</button>
   </div>
     <div class="container signin">
     <p> Dont have an account? <a href="register.php">Register </a>.</p>
     <p>  <a href="forgetpassword.php">Forget Password </a></p>
   </div>
 </form>
-<?php
 
-?>
+
+<?php
+  
+//     if(isset($_POST['submit']))
+//     {
+
+    
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $dbname = "flights";
+// $conn = new mysqli($servername, $username, $password, $dbname);
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+//   }
+//   $Email=$_POST['email'];
+//   $Password=$_POST['psw'];
+
+//   $query = "SELECT * FROM user_details WHERE Email='$Email' AND Password1='$Password'";
+//   $result = mysqli_query($conn,$query);
+//   session_start();
+//   while ($row = $result->fetch_assoc())
+//   {
+
+  
+  
+//   if($row['userType']=='1')
+//   {
+//     echo"<html><h1><a href='AdminHome.php'>AdminHome</a></html></h1>";
+//   }
+// else if($row['userType']=='2')
+//   {
+//     echo"<h1><a href='customerservice.php'>CustomerHome</a></h1>";
+//   }
+//   else if($row['userType']=='3')
+//   {
+//     echo"<html><h1><a href='QualityControlHome.php'>QualityControlHome</h1></html></a>";
+//   }
+//   else if($row['userType']=='4'&&$row['status1']=='0')
+//   {
+//    echo "<script>alert('the account is not activited');</script>";
+//   }
+//   else if($row['userType']=='4'&&$row['status1']=='1')
+//   {
+//     echo"<h1><a href='qualitycontrol.php'>UserHome</a></h1>";
+//   }
+
+//   else
+//   echo "not registerd";
+// }
+//   }
+  
+?> 
+
+  
+
 </body>
 </html>
