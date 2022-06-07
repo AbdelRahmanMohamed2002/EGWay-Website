160f8e16b32264e4408037812eb2738b9c8a9b2e
AbdelRahmanMohamed2002 <105849197+AbdelRahmanMohamed2002@users.noreply.github.com>
2022-06-07 18:37:56 +0200

    Add files via upload

---
+++ qualitycontrol.php
@@ -0,0 +1,92 @@
+<style>
+table, th, td {
+  border: 1px solid black;
+}
+
+</style>
+<?php
+include('header.php');
+//include('menubar.php');
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "flights";
+session_start();
+
+// Create connection
+$conn =  mysqli_connect($servername, $username, $password, $dbname);
+$query = "SELECT * FROM user_details where userType= 4";
+$result = mysqli_query($conn,$query);
+ 
+
+?>
+<html>
+	<title>view users</title>
+<form method="post" action="qualitycontrol.php">
+<table class="table table-bordered">
+<thead>
+<tr>
+<th> ID</th>
+	<th> Email</th>
+	<th>First Name</th>
+	<th>last name</th>
+	<th>Personal_Photo</th>
+	<th>ID_Photo</th>
+	<th>status</th>
+	<th>enable/disable</th>
+	
+  
+	 
+</tr>
+</thead>
+
+<h1>Users accounts</h1>
+</div>
+
+
+
+
+</body>
+</html>
+<?php
+ $i=0;
+while($row = mysqli_fetch_array($result)) 
+{
+    
+?>
+<tr>
+<td><?= $row['Id']; ?></td>
+	<td><?= $row['Email']; ?></td>
+	<td><?= $row['First_Name']; ?></td>
+	<td><?=  $row['Last_Name']; ?></td>
+	<td><img src="photos/<?= $row['Personal_Photo']; ?>" alt=Personal_Photo1 width=100 height=100></td>
+	<td><img src="photos/<?= $row['ID_Photo']; ?>" alt=ID_Photo1 width=100 height=100></td>
+	<td><?php if($row['status1']==1)
+	echo "accepted";
+	else
+	echo "pending";?></td>
+  
+	<td> 
+    <button> <a href="showdata2.php?userid=<?php echo $row["Id"]; ?>">status</a></td>
+   <!-- <form method="post" action="qualitycontrol.php">
+  <button type ="submit" value= "true" name="submit" id="submit" >enable</button>
+  <button type ="submit" value= "false" name="submit" id="submit" >disable</button>
+  </form>   -->
+</td>
+  
+	 
+</tr>
+<?php
+// $x=$_POST['submit'];
+// $y=$row['Email'];
+// $sq="update user_details set status=$x  where Email=$y";
+
+// $res=mysqli_query($conn,$sq);
+$i++;
+
+}
+?>
+</table>
+</form>
+</body>
+</html>
---
+++ showdata2.php
@@ -0,0 +1,42 @@
+<?php
+ include('header.php');
+ $servername = "localhost";
+ $username = "root";
+ $password = "";
+ $dbname = "flights";
+ session_start();
+ 
+ // Create connection
+ $conn = new mysqli($servername, $username, $password, $dbname);
+if(isset($_POST['submit'])) {
+  $sql="UPDATE user_details set status='" . $_POST['status']  . "' WHERE Id='" .  $_GET['userid'] . "'";
+ 
+    mysqli_query($conn,$sql);
+$message = "Record Modified Successfully";
+}
+$result = mysqli_query($conn,"SELECT * FROM user_details WHERE Id='" .  $_GET['userid'] . "'");
+$row= mysqli_fetch_array($result);
+?>
+<html>
+<head>
+<title> enable/disable users</title>
+</head>
+<body>
+<form name="frmUser" method="post" action="">
+<div><?php if(isset($message)) { echo $message; } ?>
+</div>
+<div style="padding-bottom:5px;">
+ 
+</div>
+
+ 
+status:<br>
+<select class="form-select" aria-label=".form-select-lg example" name="status" id="status" >
+  <option value="1"> enable </option>
+  <option value="0"> disable </option>
+ <br>
+ <br>
+<input type="submit" name="submit" value="Submit" class="buttom">
+</form>
+<button><a href="qualitycontrol.php">view users</button>
+<?php include ('footer.php');?>
