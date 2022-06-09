5a053a6ce52274c23d558b7c0f98a9c904430e78
Yousse23Ahmed <106341665+Yousse23Ahmed@users.noreply.github.com>
2022-06-10 00:14:40 +0200

    Add files via upload

---
+++ mainuser.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<meta charset="utf-8" />
+ <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+ <link rel="stylesheet" href="mainn.css" />
+    <title> main page</title>
+</head>
+<section class=" cards" id="makingorder">
+<center>
+<p class="font">
+Top Destination
+</p>
+<hr class="line">
+<div class="content">
+<div class="card">
+<div class="icon">
+<a href="hurda.php"><img src="3.jpeg" width="100%" height="100%"/></a>
+</div>
+</div>
+<div class="card">
+<div class="icon">
+<a href="sharm.php"><img src="2.jpeg" width="100%" height="100%"/></a>
+</div>
+</div>
+<div class="card">
+<div class="icon">
+<a href="luxor.php"><img src="1.jpeg" width="100%" height="100%"/></a>
+</div>
+</div>
+</div>
+  
+</section>
+<section>
+<center>
+<button  style="background:transparent;border:none;"  ><a href="main.php" style="font-size:25px; text-decoration:none; margin-top:25px;">return home</a></button>
+</center>
+</section>
+</body>
+</html>
