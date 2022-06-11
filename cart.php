ef5bdf203ac8746db35829779ce8fb7e7873daa1
Yousse23Ahmed <106341665+Yousse23Ahmed@users.noreply.github.com>
2022-06-11 22:37:47 +0200

    Add files via upload

--- cart.php
+++ cart.php
@@ -1,82 +1,136 @@
-
-<?php 
-session_start();
-include('connectdatabase.php'); 
-include('header.php'); 
-include('nav.php');
-// $con=mysqli_connect("localhost","root","","flights");
-// if(isset($_POST['submit']))
-// {
-
-// $date=$_POST['date'];
-// $returnaf= $_POST['returnafterthan7'];
-// $returnless= $_POST['returnlessthan7'];
-// $dat= $_POST['dat'];
-// $Time= $_POST['Time'];
-// $Tim= $_POST['Tim'];
-// $Adults= $_POST['Adults'];
-// $child= $_POST['child'];
-// $infant= $_POST['infant'];
-// $class= $_POST['class'];
-// $From= $_POST['From'];
-// $query="INSERT INTO `user_flight_details` ( `DepartureDate`, `returnlessthan7`, `returnafterthan7`, `ReturnDate`, `DepartureTime`, `ReturnTime`, `NumberofAdults`, `NumberofChildren`, `NumberofInfants`, `Class`, `from1`, `to1`) 
-//  VALUES ('$date', '$returnless', '$returnaf', '$dat', '$Time', '$Tim', '$Adults', '$child', '$infant', '$class', '$From',)";
-// $qurey_run=mysqli_query($con,$query);
-// $sq="select* from user_flight_details";
-// $r1=$con->query($sq);
-// }
-
-?>
-
- 
-<div class="container">
-<?php
-
-?>
-<?php 
-if(!isset($_SESSION['cart'])){
-	$_SESSION['cart'] = 0;
+<html>
+<head>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</head>
+<body>
+<style>
+a
+{
+text-decoration:none;
+color:black;
 }
-$items = $_SESSION['cart'];
-$cartitems = explode(",", $items);
-?>
-	<div class="row">
-	  <table class="table">
-	  	<tr>
-	  		<th>S.NO</th>
-	  		<th>Item Name</th>
-	  		<th>Price</th>
-	  	</tr>
-		<?php
-		$total = 0;
-		$i=1;
-		 foreach ($cartitems as $key=>$id) {
-			$sql = "SELECT * FROM trip WHERE id = '$id'";
-			$res=$connection->query($sql);
-			$r = mysqli_fetch_assoc($res);
-			if($r === null){
-				die();
-			}
-		?>	  	*($r1['Adults']+$r1['infant']+$r1['child'])
-	  	<tr>
-	  		<td><?php echo $i; ?></td>
-	  		<td><a href="delcart.php?remove=<?php echo $key; ?>">Remove</a> <?php echo $r['name']; ?></td>
-	  		<td>$<?php echo $r['price'] ?></td>
-	  	</tr>
-		<?php 
-			$total = $total + $r['price'];
-			$i++; 
-			} 
-		?>
-		<tr>
-			<td><strong>Total Price</strong></td>
-			<td><strong>$<?php echo $total; ?></strong></td>
-			<td><a href="checkout.php" class="btn btn-info">Checkout</a></td>
-		</tr>
-	  </table>
-	  
-	</div>
+</style>
+<section>
+<table class="table" id="mytable">
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Trip</th>
+      <th scope="col">email</th>
+      <th scope="col">price</th>
+      <th scope="col">Delete</th>
+    </tr>
+  </thead>
+    <tbody>
+ <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "flights";
+$a=$_SESSION['EMAIL'];
+$conn = new mysqli($servername, $username, $password, $dbname);
+ if(isset($a))
+ {
+  $delete=mysqli_query($conn,"DELETE FROM `sharm` WHERE `email`='$a'");
+ }
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
  
-</div>
+$sql = " SELECT (SELECT SUM((Adults500+infant200+child*400)) FROM sharm WHERE email=$a) as total;";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+ echo " <tr><th>1</th>";
+ echo "<td>sharm</td>";
+ echo "<td>".$row['email']."</td>";
+ echo "<td>".$row["total"]."</td>";
+echo "<td><a href='cart.php?'>".$row['email']."delete </a> </tr>";
+
+}
+}
+$conn->close(); 
+  ?>
+  
+  <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "flights";
+$b=$_SESSION['EMAIL'];
+$conn = new mysqli($servername, $username, $password, $dbname);
+ if(isset($b))
+ {
+  $delete=mysqli_query($conn,"DELETE FROM `hurda` WHERE `email`='$b'");
+ }
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
  
-<?php include('footer.php') ?>
+$sql = " SELECT (SELECT SUM((Adults500+infant200+child*400)) FROM hurda WHERE email=$b) as total;";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+ echo " <tr><th>2</th>";
+ echo "<td>hurda</td>";
+ echo "<td>".$row['email']."</td>";
+ echo "<td>".$row["total"]."</td>";
+echo "<td><a href='cart.php?'>".$row['email']."delete </a> </tr>";
+}
+}
+$conn->close(); 
+  ?>
+   <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "flights";
+$c=$_SESSION['EMAIL'];
+$conn = new mysqli($servername, $username, $password, $dbname);
+ if(isset($c))
+ {
+  $delete=mysqli_query($conn,"DELETE FROM `luxor` WHERE `email`='$c'");
+ }
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+$sql = " SELECT (SELECT SUM((Adults500+infant200+child*400)) FROM luxor WHERE email=$c) as total;";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+ echo " <tr><th>1</th>";
+ echo "<td>sharm</td>";
+ echo "<td>".$row['email']."</td>";
+ echo "<td>".$row["total"]."</td>";
+echo "<td><a href='cart.php?'>".$row['email']."delete </a> </tr>";
+}
+}
+$conn->close(); 
+  ?>
+  </tbody>
+</table>
+</section>
+<section>
+<center>
+  <p>
+<span>Total </span>
+<span>100</span>
+</p>
+</section>
+</center>
+<section>
+<center>
+ <button  class="btn"><a href="visa.html"> GO TO THE CHECK OUT</a> </button> 
+</section>
+</center>
+</body>
+</html>
